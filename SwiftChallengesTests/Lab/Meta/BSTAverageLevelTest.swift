822a99a456c78ac2bd5b8e5668ed30a2e5fd610a
KyleLearnedThis <kyle.learned.this@gmail.com>
2022-05-02 16:26:40 -0700

    BSTAverageLevel DFS implementation

--- SwiftChallengesTests/Lab/Meta/BSTAverageLevelTest.swift
+++ SwiftChallengesTests/Lab/Meta/BSTAverageLevelTest.swift
@@ -8,12 +8,21 @@
 import XCTest
 
 class BSTAverageLevelTest: XCTestCase {
-    func testBasicV01() throws {
+    func testBasicBFS01() throws {
         let tree = HeightBalancedTree()
         let root = tree.sortedArrayToBST(nums: [1,2,3,4,5,6,7])
         BinaryTreePrinter.printNode(root: root!)
         let expected = [4.0, 4.0, 4.0]
-        let actual = BSTAverageLevel.averageOfLevels(root)
+        let actual = BSTAverageLevel.bfs(root)
+        XCTAssertEqual(expected, actual)
+    }
+
+    func testBasicDFS01() throws {
+        let tree = HeightBalancedTree()
+        let root = tree.sortedArrayToBST(nums: [1,2,3,4,5,6,7])
+        BinaryTreePrinter.printNode(root: root!)
+        let expected = [4.0, 4.0, 4.0]
+        let actual = BSTAverageLevel.dfs(root)
         XCTAssertEqual(expected, actual)
     }
 }
