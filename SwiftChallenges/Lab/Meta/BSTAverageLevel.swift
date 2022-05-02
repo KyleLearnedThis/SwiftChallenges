eaa4168a4d947f2f67fb8cecf310b9cda06fd8cc
KyleLearnedThis <kyle.learned.this@gmail.com>
2022-05-02 16:26:59 -0700

    BSTAverageLevel DFS implementation (#28)

--- SwiftChallenges/Lab/Meta/BSTAverageLevel.swift
+++ SwiftChallenges/Lab/Meta/BSTAverageLevel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class BSTAverageLevel {
-    static func averageOfLevels(_ root: TreeNode<Int>?) -> [Double] {
+    static func bfs(_ root: TreeNode<Int>?) -> [Double] {
         var result = [Double]()
         var queue = [TreeNode<Int>?]()
 
@@ -35,5 +35,56 @@ class BSTAverageLevel {
         }
 
         return result
+    }
+
+    static func dfs(_ root: TreeNode<Int>?) -> [Double] {
+        var result = [Double]()
+        var map = [Int: [Int]]()
+
+        dfsWork(0, root, &map)
+
+        let list = map.sorted { $0.0 < $1.0 }
+
+        for item in list {
+            let key = item.key
+            let value = item.value
+            print("[\(key)]->[\(value)] ")
+            let currentAverage = value.reduce(0,+) / value.count
+            result.append(Double(currentAverage))
+        }
+
+        return result
+    }
+
+    static func dfsWork(_ level: Int, _ node: TreeNode<Int>?, _ map: inout [Int: [Int]]) {
+        if node == nil {
+            return
+        }
+
+        var levelList: [Int] = map[level, default: [Int]()]
+        let data = node!.data
+        levelList.append(data)
+        map[level] = levelList
+
+        dfsWork(level+1, node?.left, &map)
+        dfsWork(level+1, node?.right, &map)
+
+    }
+
+    static func height(node: TreeNode<Int>?) -> Int {
+        if node == nil {
+            return 0
+        } else {
+            let left = node?.left
+            let right = node?.right
+            let lheight = height(node: left)
+            let rheight = height(node: right)
+
+            if lheight > rheight {
+                return lheight + 1
+            } else {
+                return rheight + 1
+            }
+        }
     }
 }
