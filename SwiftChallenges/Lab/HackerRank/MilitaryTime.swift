6814e1ab079e0fd44f0c6433c06721598032fbc1
KyleLearnedThis <kyle.learned.this@gmail.com>
2022-05-10 16:49:22 -0700

    Remove commented out codes

--- SwiftChallenges/Lab/HackerRank/MilitaryTime.swift
+++ SwiftChallenges/Lab/HackerRank/MilitaryTime.swift
@@ -46,13 +46,3 @@ class MilitaryTime {
         return result
     }
 }
-
-
-//extension String {
-//    subscript(_ range: NSRange) -> String {
-//        let start = self.index(self.startIndex, offsetBy: range.lowerBound)
-//        let end = self.index(self.startIndex, offsetBy: range.upperBound)
-//        let subString = self[start..<end]
-//        return String(subString)
-//    }
-//}
