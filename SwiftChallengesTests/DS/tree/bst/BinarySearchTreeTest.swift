d64367946983a5bf70f5268b03ff1e75fbb8cc1a
KyleLearnedThis <kyle.learned.this@gmail.com>
2022-06-13 00:26:41 -0700

    Code refactoring, add comments (#54)

--- SwiftChallengesTests/DS/tree/bst/BinarySearchTreeTest.swift
+++ SwiftChallengesTests/DS/tree/bst/BinarySearchTreeTest.swift
@@ -12,35 +12,35 @@ class BinarySearchTreeTest: XCTestCase {
     func testPreOrder() {
         let input = [15, 6, 18, 3, 7, 17, 20, 2, 4, 13, 9]
         let bst = BinarySearchTree<Int>(input: input)
+        BinaryTreePrinter.printNode(root: bst.root!)
         bst.printPreOrder()
-        BinaryTreePrinter.printNode(root: bst.getRoot()!)
     }
 
     func testInOrder() {
         let input = [15, 6, 18, 3, 7, 17, 20, 2, 4, 13, 9]
         let bst = BinarySearchTree<Int>(input: input)
+        BinaryTreePrinter.printNode(root: bst.root!)
         bst.printInOrder()
-        BinaryTreePrinter.printNode(root: bst.getRoot()!)
     }
 
     func testPostOrder() {
         let input = [15, 6, 18, 3, 7, 17, 20, 2, 4, 13, 9]
         let bst = BinarySearchTree<Int>(input: input)
+        BinaryTreePrinter.printNode(root: bst.root!)
         bst.printPostOrder()
-        BinaryTreePrinter.printNode(root: bst.getRoot()!)
     }
 
     func testLevelOrder() {
         let input = [15, 6, 18, 3, 7, 17, 20, 2, 4, 13, 9]
         let bst = BinarySearchTree<Int>(input: input)
-        BinaryTreePrinter.printNode(root: bst.getRoot()!)
+        BinaryTreePrinter.printNode(root: bst.root!)
         bst.printLevelOrder()
     }
 
     func testSearch() {
         let input = [15, 8, 30, 4, 10, 20, 40]
         let bst = BinarySearchTree<Int>(input: input)
-        BinaryTreePrinter.printNode(root: bst.getRoot()!)
+        BinaryTreePrinter.printNode(root: bst.root!)
         let seek = 40
         let actual = bst.search(searchValue: seek)
 
